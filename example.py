f820dcedbaba3ced6cbefb673d77cb9a037577f2
XiaoBaiYun <71159641+littlewhitecloud@users.noreply.github.com>
2023-05-20 20:12:38 +0800

    first example upload

---
+++ example.py
@@ -0,0 +1,9 @@
+from CustomTkinterTitlebar import CTT
+
+example = CTT()
+example.titlebarconfig(height = 40)
+example.usetitle(False)
+example.useicon(False)
+example.useblur()
+
+example.mainloop()
