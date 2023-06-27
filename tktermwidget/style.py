1bf88a0c4b5a267b7a28cb6bec380de43e06104f
XiaoBaiYun <71159641+littlewhitecloud@users.noreply.github.com>
2023-06-27 19:45:19 +0800

    update a bit of ui

--- tktermwidget/style.py
+++ tktermwidget/style.py
@@ -1,6 +1,7 @@
 """Styles for terminal"""
 import json
 from pathlib import Path
+from tkinter import Text, Tk
 
 from platformdirs import user_cache_dir
 
@@ -26,19 +27,51 @@ if not (JSON_FILE).exists():
 #    "foreground": "{yourhexcolor}",
 # }
 
+
 # Styles creater
 def writestyle(**styles) -> None:
     """Write the style into the json file"""
     # User can use this function to write with their own style
     # TODO: improve it, new style will overwrite the old style
     with open(JSON_FILE, "w", encoding="utf-8") as json_obj:
-        json.dump(styles, json_obj)
+        json.dump(styles, json_obj, indent=1)
+
 
 def load_style() -> dict:
     """Get style from the json file"""
     # User also can use this function to get their own style
     with open(JSON_FILE, "r", encoding="utf-8") as json_obj:
         return json.load(json_obj)
+
+
+def config_style() -> None:
+    """Config the style with the ui"""
+    config = Tk()
+    config.geometry("685x475")
+    config.title("Config your style")
+
+    style: dict = Powershell  # TODO: Replace it
+
+    # TODO: Add buttons and labels to help user to config the style
+    render = Text(
+        config,
+        background=style["background"],
+        insertbackground=style["insertbackground"],
+        selectbackground=style["selectbackground"],
+        selectforeground=style["selectforeground"],
+        foreground=style["foreground"],
+        font=("Cascadia Mono", 9, "normal"),
+    )
+
+    # TODO: Improve here
+    render.insert("insert", "This is a normal text for test style.")
+    render.tag_add("select", "1.31", "1.36")
+    render.tag_config("select", background=style["selectbackground"], foreground=style["selectforeground"])
+    render["state"] = "disable"
+
+    render.pack(side="right", fill="y")
+    config.mainloop()
+
 
 # Built-in styles
 Default: dict = {  # Style for normal tkterminalwidget
@@ -73,10 +106,10 @@ Git: dict = {
     "foreground": "#efefef",
 }
 
-# User custom function
+# User custom style
 Custom: dict = load_style()
 
-"""
-writestyle(background = "#2B2B2B", insertbackground = "#DCDCDC", 
-selectbackground = "#b4b3b3", selectforeground = "#e6e6e6", foreground = "#cccccc")
-"""
+# writestyle(background = "#2B2B2B", insertbackground = "#DCDCDC",
+# selectbackground = "#b4b3b3", selectforeground = "#e6e6e6", foreground = "#cccccc")
+
+config_style()
