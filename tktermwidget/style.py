53b337bda19dba28dd229fe50e538b6c41c3630f
XiaoBaiYun <71159641+littlewhitecloud@users.noreply.github.com>
2023-06-27 15:57:51 +0800

    add ```write_style``` and ```load_style```

--- tktermwidget/style.py
+++ tktermwidget/style.py
@@ -1,5 +1,32 @@
 """Styles for terminal"""
+import json
+from platformdirs import user_cache_dir
+from pathlib import Path
 
+# Format:
+# {yourstylename}: dict = {
+#    "background": "{yourhexcolor}",        
+#    "insertbackground": "{yourhexcolor}",
+#    "selectbackground": "{yourhexcolor}",
+#    "selectforeground": "{yourhexcolor}",
+#    "foreground": "{yourhexcolor}",
+# }
+
+# Constants
+STYLE_PATH = Path(user_cache_dir("tktermwidget"))
+JSON_FILE = STYLE_PATH / "styles.json"
+
+if not STYLE_PATH.exists():
+    STYLE_PATH.mkdir(parents=True)
+    with open(JSON_FILE, "w", encoding="utf-8") as f:
+        f.close()
+
+if not (JSON_FILE).exists():
+    with open(JSON_FILE, "w", encoding="utf-8") as f:
+        f.close()
+
+
+# Built-in styles
 Default: dict = {  # Style for normal tkterminalwidget
     "background": "#2B2B2B",
     "insertbackground": "#DCDCDC",
@@ -23,5 +50,18 @@ Command: dict = {  # Style for normal "cmd.exe"
     "selectforeground": "#000000",
     "foreground": "#f2f2f2",
 }
-
 # TODO: add a user custom style function later...
+
+# Styles creater
+def writestyle(**styles) -> None:
+	with open(JSON_FILE, "w", encoding = "utf-8") as json_obj:
+		json.dump(styles, json_obj)
+
+def load_style() -> dict:
+	with open(JSON_FILE, "r", encoding = "utf-8") as json_obj:
+		return json.load(json_obj)
+
+#writestyle(background = "#2B2B2B", insertbackground = "#DCDCDC", selectbackground = "#b4b3b3", selectforeground = "#e6e6e6", foreground = "#cccccc")
+
+Custom: dict = load_style()
+#print(Custom)
