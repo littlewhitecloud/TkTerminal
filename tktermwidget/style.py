e235cffca17e1fbecf9c3068a10d91018823e73e
XiaoBaiYun <71159641+littlewhitecloud@users.noreply.github.com>
2023-06-28 10:54:00 +0800

    not bad

--- tktermwidget/style.py
+++ tktermwidget/style.py
@@ -1,7 +1,6 @@
 """Styles for terminal"""
 import json
 from pathlib import Path
-from tkinter import Text, Tk
 
 from platformdirs import user_cache_dir
 
@@ -29,7 +28,7 @@ if not (JSON_FILE).exists():
 
 
 # Styles creater
-def writestyle(**styles) -> None:
+def write_style(**styles) -> None:
     """Write the style into the json file"""
     # User can use this function to write with their own style
     # TODO: improve it, new style will overwrite the old style
@@ -46,15 +45,88 @@ def load_style() -> dict:
 
 def config_style() -> None:
     """Config the style with the ui"""
+    # TODO: improve the code
+    # TODO: improve the ui
+    # TODO: fix some logic bug
+    # TODO: ...
+    from tkinter import Tk, Text
+    from tkinter.ttk import Label, Entry, Frame, Button
+    from tkinter.colorchooser import askcolor
+
+    def selectcolor(entry: Entry, name: str) -> None:
+        color = askcolor()[-1]
+        entry.delete(0, "end")
+        entry.insert("insert", color)
+        style[name] = color
+        renderupdate()
+    
+    def renderupdate() -> None:
+        render.config(
+            background=style["background"],
+            insertbackground=style["insertbackground"],
+            selectbackground=style["selectbackground"],
+            selectforeground=style["selectforeground"],
+            foreground=style["foreground"],
+        )
+        render.tag_config("select", background=style["selectbackground"], foreground=style["selectforeground"])
+        config.update()
+    
+    def savestyle() -> None:
+        write_style(
+            background=style["background"],
+            insertbackground=style["insertbackground"],
+            selectbackground=style["selectbackground"],
+            selectforeground=style["selectforeground"],
+            foreground=style["foreground"],
+        )
+        config.destroy()
+
     config = Tk()
-    config.geometry("685x475")
+    config.geometry("825x500")
     config.title("Config your style")
+    
+    try: from sv_ttk import set_theme 
+    except: pass
+    else: 
+        set_theme("light") 
+        config.option_add("*font", ("Cascadia Mono", 9))
 
-    style: dict = Powershell  # TODO: Replace it
+    style: dict = load_style()
+    
+    create = Label(config, text = "Create your custom style!")
 
+    backgroundframe = Frame(config)
+    background = Label(backgroundframe, text = "Choose or input your background hex color")
+    backgroundentry = Entry(backgroundframe)
+    backgroundbutton = Button(backgroundframe, text = "...", width = 3, command = lambda: selectcolor(backgroundentry, "background"))
+    
+    insertbackgroundframe = Frame(config)
+    insertbackground = Label(insertbackgroundframe, text = "Choose or input your insertbackground hex color")
+    insertbackgroundentry = Entry(insertbackgroundframe)
+    insertbackgroundbutton = Button(insertbackgroundframe, text = "...", width = 3, command = lambda: selectcolor(insertbackgroundentry, "insertbackground"))
+
+    selectbackgroundframe = Frame(config)
+    selectbackground = Label(selectbackgroundframe, text = "Choose or input your selectbackground hex color")
+    selectbackgroundentry = Entry(selectbackgroundframe)
+    selectbackgroundbutton = Button(selectbackgroundframe, text = "...", width = 3, command = lambda: selectcolor(selectbackgroundentry, "selectbackground"))
+
+    selectforegroundframe = Frame(config)
+    selectforeground = Label(selectforegroundframe, text = "Choose or input your selectforeground hex color")
+    selectforegroundentry = Entry(selectforegroundframe)
+    selectforegroundbutton = Button(selectforegroundframe, text = "...", width = 3, command = lambda: selectcolor(selectforegroundentry, "selectforeground"))
+
+    foregroundframe = Frame(config)
+    foreground = Label(foregroundframe, text = "Choose or input your selectforeground hex color")
+    foregroundentry = Entry(foregroundframe)
+    foregroundbutton = Button(foregroundframe, text = "...", width = 3, command = lambda: selectcolor(foregroundentry, "foreground"))
+    
+    save = Button(config, text = "Save", command = savestyle)
+    cancel = Button(config, text= "Cancel", command = config.destroy)
+    
     # TODO: Add buttons and labels to help user to config the style
     render = Text(
         config,
+        width=40,
         background=style["background"],
         insertbackground=style["insertbackground"],
         selectbackground=style["selectbackground"],
@@ -70,6 +142,36 @@ def config_style() -> None:
     render["state"] = "disable"
 
     render.pack(side="right", fill="y")
+    create.pack(side = "top", fill = "y", pady = 15)
+
+    background.pack(side = "left", fill = "y")
+    backgroundentry.pack(side = "left")
+    backgroundbutton.pack(side = "left")
+    backgroundframe.pack(side = "top", fill = "y")
+
+    insertbackground.pack(side = "left", fill = "y")
+    insertbackgroundentry.pack(side = "left")
+    insertbackgroundbutton.pack(side = "left")
+    insertbackgroundframe.pack(side = "top", fill = "y")
+
+    selectbackground.pack(side = "left", fill = "y")
+    selectbackgroundentry.pack(side = "left")
+    selectbackgroundbutton.pack(side = "left")
+    selectbackgroundframe.pack(side = "top", fill = "y")
+    
+    selectforeground.pack(side = "left", fill = "y")
+    selectforegroundentry.pack(side = "left")
+    selectforegroundbutton.pack(side = "left")
+    selectforegroundframe.pack(side = "top", fill = "y")
+
+    foreground.pack(side = "left", fill = "y")
+    foregroundentry.pack(side = "left")
+    foregroundbutton.pack(side = "left")
+    foregroundframe.pack(side = "top", fill = "y")
+    
+    save.pack(side = "bottom", fill = "x")
+    cancel.pack(side = "bottom", fill = "x")
+    
     config.mainloop()
 
 
@@ -109,7 +211,8 @@ Git: dict = {
 # User custom style
 Custom: dict = load_style()
 
-# writestyle(background = "#2B2B2B", insertbackground = "#DCDCDC",
+# write_style(background = "#2B2B2B", insertbackground = "#DCDCDC",
 # selectbackground = "#b4b3b3", selectforeground = "#e6e6e6", foreground = "#cccccc")
 
-config_style()
+#config_style()
+#print(Custom)
